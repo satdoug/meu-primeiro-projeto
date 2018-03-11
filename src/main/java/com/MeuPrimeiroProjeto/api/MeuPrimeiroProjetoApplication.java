8e924e11c3143d61e6fb25fc7376bf605e085ea0
satdoug <dm.santos2011@gmail.com>
2018-03-10 22:32:30 -0300

    Integração com o Travis e Teste..........

--- src/main/java/com/MeuPrimeiroProjeto/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/MeuPrimeiroProjeto/api/MeuPrimeiroProjetoApplication.java
@@ -8,6 +8,5 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-		System.out.println("Meu primeiro projeto utilizando os melhores frameworks!");
 	}
 }
