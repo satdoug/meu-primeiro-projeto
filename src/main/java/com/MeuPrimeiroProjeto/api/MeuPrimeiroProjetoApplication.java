88e3fe2b6423e2d0253c9566bd1aff2a6e835e73
satdoug <dm.santos2011@gmail.com>
2018-03-10 22:17:58 -0300

    Integração com o Travis e Teste....

--- src/main/java/com/MeuPrimeiroProjeto/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/MeuPrimeiroProjeto/api/MeuPrimeiroProjetoApplication.java
@@ -9,14 +9,6 @@ public class MeuPrimeiroProjetoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("Meu primeiro projeto utilizando os melhores frameworks!");
-		System.out.println("Teste Douglas");
-		System.out.println("Teste Douglas");
-		System.out.println("Teste Douglas");
-		System.out.println("Teste Douglas");
-		System.out.println("Teste Douglas");
-		System.out.println("Teste Douglas");
-		System.out.println("Teste Douglas");
-		System.out.println("Teste Douglas");
-		
+		System.out.println("Teste Douglas");	
 	}
 }
